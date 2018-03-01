96680bd4494e9d2bb2ffe2ca9da1eac6545cd9df
ayang10 <ayang014@gmail.com>
2018-03-01 07:04:34 -0500

    thursday updates
    added gulpfile, scss, and starting to bring it necessary packages

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+﻿var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    minifyCSS = require('gulp-minify-css');
+
+gulp.task('sass', function () {
+    return gulp.src('./assets/sass/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./assets/css'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('./assets/sass/*.scss', ['sass']);
+});
+
+gulp.task('minify-css', function () {
+    return gulp.src('./assets/css/style.css')
+        .pipe(minifyCSS({ keepSpecialComments: 1 }))
+        .pipe(gulp.dest('./assets/minify-css'));
+});
+
+gulp.task('default', ['sass', 'watch']);
