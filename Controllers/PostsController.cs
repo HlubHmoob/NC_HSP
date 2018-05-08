32959f8cc9c0405bc49ac8d5ec0234b228b72ff1
ayang10 <ayang014@gmail.com>
2018-05-08 07:41:04 -0400

    tuesday commit

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NC_HSP.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,38 @@ namespace NC_HSP.Controllers
 {
     public class PostsController : Controller
     {
-        // GET: Posts
+        // *** GET: All Posts ***
         public ActionResult Index()
+        {
+            ViewBag.Posts = "Posts";
+            PostsDBHandler PHandler = new PostsDBHandler();
+            ModelState.Clear();
+
+            return View(PHandler.GetPostItem());
+        }
+
+        // *** Add new Post ***
+        [HttpGet]
+        public ActionResult Create()
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Create(PostsViewModels iPost)
+        {
+            if (ModelState.IsValid)
+            {
+                PostsDBHandler PHandler = new PostsDBHandler();
+                if (PHandler.PostItem(iPost))
+                {
+                    ViewBag.Message = "Item Added Successfully";
+                    ModelState.Clear();
+                }
+            }
+            return View();
+        }
+
+
     }
 }
