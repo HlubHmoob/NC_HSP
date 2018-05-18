27afb9f6c8d3afe1d730a30c4c3b6528c902658e
ayang10 <ayang014@gmail.com>
2018-05-17 20:27:17 -0400

    crazy thrusday commit
    didn't add .sln that's why!!!

---
+++ NC_HSP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NC_HSP.Startup))]
+namespace NC_HSP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
